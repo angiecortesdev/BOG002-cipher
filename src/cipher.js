6455fa80186034d655ef4dbb3be617df61b678a1
angiecortesdev <angiecortes29@gmail.com>
2021-02-26 12:14:08 -0500

    se agrego decode en el cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,11 @@
 const cipher = {
-    // ...
+    decode: (string, offset) => {
+        if (!string)
+            return '';
+        const letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+        offset = (offset % 26 - 26) % 26;
+        return string.replace(/[A-Z]/ig, c => letras[(letras.indexOf(c) - offset) % 26]);
+    }
 };
 
 export default cipher;
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,17 @@
-import cipher from '../cipher.js';
+import cipher from '../cipher';
 
-console.log(cipher);
+window.addEventListener("load", inicio, true);
+
+
+function inicio() {
+    document.getElementById("mensaje").addEventListener("keyup", function() {
+        // eslint-disable-next-line no-undef
+        mensaje.value = mensaje.value.toUpperCase();
+    }, true);
+
+    document.getElementById("descifrar").addEventListener("click", function() {
+        let string = document.getElementById("mensaje").value;
+        let offset = document.getElementById("desplazamiento").value;
+        document.getElementById("mensaje2").value = cipher.decode(string, offset);
+    }, true);
+}
